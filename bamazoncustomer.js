9db16d2da4a01fe4b8fd32a3c63fd4caaf09302a
Ruben Valdez <ruben.valdez7@gmail.com>
2019-07-19 19:53:30 -0700

    prompt resubmits query:

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -24,7 +24,7 @@ function afterConnection() {
  connection.query("SELECT * FROM products", function(err, res) {
    if (err) throw err;
    displayResults(res)
-   
+   userPrompt(res)
    connection.end();
  });
 }
@@ -37,4 +37,63 @@ for(i=0; i < res.length; i++){
    console.log("Price $" + res[i].price)
    console.log(res[i].stock_quantity + " left in stock")
 }
+}
+
+function userPrompt(){
+  var inquirer = require("inquirer");
+inquirer
+ .prompt([
+   {
+       type:"input",
+       message: "Which item would you like to purchase",
+       name:"item",
+   },
+   {
+    type:"input",
+    message: "How many would you like?",
+    name:"quantity",
+},
+
+ ])
+ .then(function(response){
+   console.log(response.item,response.quantity)
+   
+  checkQuantity(response.item,response.quantity)
+
+   if(response.confirm){
+       console.log(response)
+   }
+ })
+
+}
+
+function checkQuantity(item, quantity){
+  console.log(item + " " + quantity)
+  var query = "SELECT * FROM products where item_id = " + item;
+  var connection = mysql.createConnection({
+    host: "localhost",
+   
+    // Your port; if not 3306
+    port: 3306,
+   
+    // Your username
+    user: "root",
+   
+    // Your password
+    password: "Monkey",
+    database: "bamazon_db"
+   });
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    newConnection(query);
+   });
+  function newConnection(query) {
+    console.log(query)
+    connection.query(query, function(err, res) {
+      if (err) throw err;
+      displayResults(res)
+     
+    });
+   }
 }
